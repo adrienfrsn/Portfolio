d8b1effef50ff6c53a11adc146555b7af28fccad
Adrien Fryson <adrien.fryson@outlook.com>
2024-11-04 11:12:10 +0100

    ajout compétences ciblées projets

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 
 menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+        setTimeout(() => {
+            mobileMenu.classList.add('show');
+        }, 10);
+    } else {
+        mobileMenu.classList.remove('show');
+        setTimeout(() => {
+            mobileMenu.classList.add('hidden');
+        }, 500);
+    }
 });
